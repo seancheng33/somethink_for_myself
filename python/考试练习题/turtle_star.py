33ee90b2696dafed117bf794445ac36dd1692cb0
seancheng33 <seancheng33@gmail.com>
2018-07-26 15:10:16 +0800

    考试练习

---
+++ python/考试练习题/turtle_star.py
@@ -0,0 +1,21 @@
+'''
+@Author        : sean cheng
+@Email         : aya234@163.com
+@Create_Time   : 2018/7/26 
+@Program       : turtle 绘制红色的五角星
+'''
+
+from turtle import *
+
+setup(400, 400)
+penup()
+goto(-100, 50)
+pendown()
+color('red')
+begin_fill()
+for i in range(5):
+    forward(200)
+    right(144)
+end_fill()
+hideturtle()
+done()
