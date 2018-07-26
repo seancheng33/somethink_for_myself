5034f430b7d5a4fed6c5f0fc41a3103b8b559495
seancheng33 <seancheng33@gmail.com>
2018-07-26 14:58:00 +0800

    考试练习题

---
+++ python/考试练习题/turtle_square.py
@@ -0,0 +1,15 @@
+'''
+@Author        : sean cheng
+@Email         : aya234@163.com
+@Create_Time   : 2018/7/26 
+@Program       : turtle 绘制正方形螺旋线
+'''
+
+import turtle
+
+n = 10
+for i in range(1, 10, 1):
+    for j in [90, 180, -90, 0]:
+        turtle.seth(j)
+        turtle.fd(n)
+        n += 5
--- shell/restart_mqnode.sh
+++ shell/restart_mqnode.sh
@@ -8,4 +8,5 @@ do
 kill -9 $id  
 echo "killed $id"  
 done
+rm -rf /home/stvasms/mcs/ActiveMQ4.1.2/node1/activemq-data/kaha.db
 nohup sh /home/stvasms/mcs/ActiveMQ4.1.2/node1/bin/activemq &
