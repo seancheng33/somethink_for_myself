e7f79221f272f4dcac04b9e2fd5458fc6982b333
seancheng33 <seancheng33@gmail.com>
2018-07-27 10:52:49 +0800

    考试练习题

---
+++ python/考试练习题/turtle_CctCircle.py
@@ -0,0 +1,20 @@
+'''
+@Author        : sean cheng
+@Email         : aya234@163.com
+@Create_Time   : 2018/7/26
+@Program       : turtle 绘制同心圆
+'''
+
+import turtle as t
+
+def DrawCctCircle(n):
+    t.penup()
+    t.goto(0, -n)
+    t.pendown()
+    t.circle(n)
+
+for i in range(20,100,20):
+    DrawCctCircle(i)
+
+t.hideturtle()
+t.done()
