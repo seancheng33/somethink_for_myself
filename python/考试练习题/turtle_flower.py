53bf6fdee16a354abe52f7e32e14ece9773e8b09
seancheng33 <seancheng33@gmail.com>
2018-08-22 11:15:24 +0800

    考试练习

---
+++ python/考试练习题/turtle_flower.py
@@ -0,0 +1,14 @@
+'''
+@Author        : sean cheng
+@Email         : aya234@163.com
+@Create_Time   : 2018/8/22 
+@Program       : turtle 绘制花
+'''
+
+import turtle
+
+for i in range(4):
+	turtle.right(90)
+	turtle.circle(50,180)
+
+turtle.done()
