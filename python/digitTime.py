fdf27a712d92b62013e18cd1741bf4d74fa8855d
seancheng33 <seancheng33@gmail.com>
2018-09-13 11:47:32 +0800

    turtle练习

---
+++ python/digitTime.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 10:55:29 2018
+
+@author: sean
+"""
+
+import turtle as t
+
+def drawline(draw):
+    t.pendown() if draw else t.penup()
+    t.fd(40)
+    t.right(90)
+    
+    
+def drawDigit(digit):
+    drawline(True) if digit in [2,3,4,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,1,3,4,5,6,7,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,3,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,6,8] else drawline(False)
+    t.left(90)
+    drawline(True) if digit in [0,4,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,3,5,6,7,8,9] else drawline(False)
+    drawline(True) if digit in [0,1,2,3,4,7,8,9] else drawline(False)
+    t.left(180)
+    t.penup()
+    t.fd(20)
+    
+    
+def drawDate(date):
+    for i in date:
+        drawDigit(eval(i))
+
+
+def main():
+    t.setup(800,350,200,200)
+    t.penup()
+    t.fd(-300)
+    t.pensize(5)
+    drawDate('20181010')
+    t.hideturlte()
+    t.done()
+
+
+main()
---
+++ python/digitTime2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 10:55:29 2018
+
+@author: sean
+"""
+import time
+import turtle as t
+
+def drawGap():
+	t.penup()
+	t.fd(5)
+
+
+def drawline(draw):
+	drawGap()
+	t.pendown() if draw else t.penup()
+	t.fd(40)
+	drawGap()
+	t.right(90)
+
+
+def drawDigit(digit):
+	drawline(True) if digit in [2,3,4,5,6,8,9] else drawline(False)
+	drawline(True) if digit in [0,1,3,4,5,6,7,8,9] else drawline(False)
+	drawline(True) if digit in [0,2,3,5,6,8,9] else drawline(False)
+	drawline(True) if digit in [0,2,6,8] else drawline(False)
+	t.left(90)
+	drawline(True) if digit in [0,4,5,6,8,9] else drawline(False)
+	drawline(True) if digit in [0,2,3,5,6,7,8,9] else drawline(False)
+	drawline(True) if digit in [0,1,2,3,4,7,8,9] else drawline(False)
+	t.left(180)
+	t.penup()
+	t.fd(20)
+
+
+def drawDate(date):
+	t.pencolor('red')
+	for i in date:
+		if i == '-':
+			t.write('年',font=(None,18,'normal'))
+			t.pencolor('green')
+			t.fd(40)
+		elif i == '=':
+			t.write('月',font=(None,18,'normal'))
+			t.pencolor('blue')
+		elif i == '+':
+			t.write('日',font=(None,18,'normal'))
+		else:
+			drawDigit(eval(i))
+
+
+def main():
+	t.setup(800,350,200,200)
+	t.penup()
+	t.fd(-300)
+	t.pensize(5)
+	drawDate(time.strftime("%Y-%m=%d+",time.gmtime()))
+	t.hideturtle()
+	t.done()
+
+
+main()
