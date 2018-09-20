a4e4e09055d624cf517cc5e0bc9d6fc580c28e60
seancheng33 <seancheng33@gmail.com>
2018-09-20 16:21:34 +0800

    机器学习算法练习

--- Data_Analsis/kNN2.py
+++ Data_Analsis/kNN2.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 19 10:12:59 2018
-
-@author: sean
-"""
+'''
+@Author : sean cheng
+@Email  : aya234@163.com
+@Create_Time   : 2018/9/19
+@Program  : kNN算法的练习实例一则
+'''
 
 import numpy as np
 import operator
@@ -29,7 +28,61 @@ def classify0(inX,dataSet,labels,k):
     sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
 
+
+def file2mattrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = np.zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = np.zeros(np.shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals,(m,1))
+    normDataSet = normDataSet/np.tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat,datingLabels = file2mattrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print('the classifier came back with: %d, the real answer is :%d' % (classifierResult,datingLabels[i]))
+        if (classifierResult != datingLabels[i]):errorCount += 1.0
+    print('the total error rate is:%f' % (errorCount/float(numTestVecs)))
+
+
+
 if __name__ == '__main__':
-    group,labels = createDataSet()
-    print(classify0([0,0],group,labels,3))
-    
+    # group,labels = createDataSet()
+    # print(classify0([0,0],group,labels,3))
+    datingDataMat,datingLabels = file2mattrix('datingTestSet2.txt')
+    # print(datingDataMat)
+    # print(datingLabels)
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*np.array(datingLabels),15.0*np.array(datingLabels))
+    # plt.show()
+    normDataSet,ranges,minVals = autoNorm(datingDataMat)
+    # print(normDataSet)
+    # print(ranges)
+    # print(minVals)
+    print(datingClassTest())
