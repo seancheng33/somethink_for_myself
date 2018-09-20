aa5b81c04bca41c7e11b373b0948eeae98ff7a09
seancheng33 <seancheng33@gmail.com>
2018-09-20 17:07:54 +0800

    机器学习算法练习

--- MachineLearning/kNN.py
+++ MachineLearning/kNN.py
@@ -66,9 +66,18 @@ def datingClassTest():
         classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
         print('the classifier came back with: %d, the real answer is :%d' % (classifierResult,datingLabels[i]))
         if (classifierResult != datingLabels[i]):errorCount += 1.0
-    print('the total error rate is:%f' % (errorCount/float(numTestVecs)))
+    print('the total error rate is: %f' % (errorCount/float(numTestVecs)))
 
-
+def classifyPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats = float(input('percentage of time spent playing video games?'))
+    ffMiles = float(input('frequent flier miles earned per year?'))
+    iceCream = float(input('liters of ice cream consumed per year?'))
+    datingDataMat,datingLabels = file2mattrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    inArr = np.array([ffMiles,percentTats,iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print('You will probably like this person:{}'.format(resultList[classifierResult-1]))
 
 if __name__ == '__main__':
     # group,labels = createDataSet()
@@ -85,4 +94,5 @@ if __name__ == '__main__':
     # print(normDataSet)
     # print(ranges)
     # print(minVals)
-    print(datingClassTest())
+    # print(datingClassTest())
+    classifyPerson()
