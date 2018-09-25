68700ddc249803599dbc74d6fa5abf81d0713b71
seancheng33 <seancheng33@gmail.com>
2018-09-25 11:19:24 +0800

    添加决策树练习

--- MachineLearning/trees.py
+++ MachineLearning/trees.py
@@ -7,3 +7,14 @@
 from math import log
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
+    labelCounts = {}
+    for fearVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelsCounts[currentLabel] = 0
+        labelsCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob,2)
+    return shannonEnt
