a556d1d9609e1e98846e303ba45fe2f8ee667728
seancheng33 <seancheng33@gmail.com>
2018-09-25 11:30:04 +0800

    决策树练习

--- MachineLearning/trees.py
+++ MachineLearning/trees.py
@@ -18,3 +18,17 @@ def calcShannonEnt(dataSet):
         prob = float(labelCounts[key])/numEntries
         shannonEnt -= prob * log(prob,2)
     return shannonEnt
+
+
+def createDataSet():
+    dataSet = [[1, 1,'yes'],
+            [1, 1,'yes'],
+            [1, 0,'no'],
+            [0,1,'no'],
+            [0, 1,'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+if __name__ == '__main__':
+    myDat,labels=createDataSet()
+    print(myDat)
