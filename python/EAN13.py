49fcf04700f40b94faee738e3a7105425fe4275a
seancheng33 <seancheng33@gmail.com>
2018-09-30 15:46:22 +0800

    修改代码EAN-13

--- python/EAN13.py
+++ python/EAN13.py
@@ -2,10 +2,11 @@
 @Author       : sean cheng
 @Email        : aya234@163.com
 @CreateTime   : 2018/9/30
-@Program      : python基础练习题，EAN-13的校验码计算
+@Program      : python基础练习题，EAN-13的校验码计算，这个校验码计算也有使用与ISBN的
+                校验码计算
 '''
 
-strNum = '697145891104'
+strNum = '978704048861'
 indexNum = 0
 num1 = 0
 num2 = 0
@@ -20,11 +21,14 @@ result = total % 10
 if result == 0 :
 	print(0)
 else:
-    print(10 - result)
+    print(10 - result)  # 为什么这里当result等于10的时候，打印出来的是10？
 
 
 '''
+EAN-13
 6971458911047
 6901234567892
 6922266443770
+ISBN
+9787044488616
 '''
