59ef638f8219457717719869c1bfd06d8b72dcca
seancheng33 <seancheng33@gmail.com>
2018-10-25 22:19:12 +0800

    pandas脚本修改

--- python/pandas_split_row.py
+++ python/pandas_split_row.py
@@ -11,4 +11,5 @@ import pandas as pd
 df = pd.DataFrame(pd.read_csv('/Users/sean/Desktop/内容-新闻.csv'))  #读csv文件为dataframe
 new_df = df.drop('封面图片路径',axis=1).join(df['封面图片路径'].str.split('|',
                 expand=True).stack().reset_index(level=1))
-new_df.to_csv('/Users/sean/Desktop/内容-新闻_out.csv')  #将处理后的文件保存为dataframe
+#new_df.to_csv('/Users/sean/Desktop/内容-新闻_out.csv')  #将处理后的文件保存为csv文件
+new_df.to_excel('/Users/sean/Desktop/内容-新闻_out.xlsx')  #将处理后的文件保存为csv文件
