18fb1fdd209f0099c490d53012ca6c69813b940b
seancheng33 <seancheng33@gmail.com>
2019-04-04 15:14:57 +0800

    测试存活主机的脚本

---
+++ python/host_alive.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2019/4/4
+@Program      : 收集某一网段的IP段下的存活主机。
+'''
+import os
+import sys
+import re
+import time
+import subprocess
+
+lifeline = re.compile(r"(\d) received")
+report = ("No response","Partial Response","Alive")
+
+print(time.ctime())
+
+for host in range(1,255):
+	ip = '192.168.1.'+str(host)
+	pingaling = subprocess.Popen(['ping','-q','-c 2','r',ip],shell=False,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+	print("Testing",ip,)
+
+	while 1:
+		pingaling.stdout.flush()
+		line=pingaling.stdout.readline()
+		if not line:
+			break
+		igot = re.findall(lifeline,str(line))
+		if igot:
+			print(report[int(igot[0])])
+print(time.ctime())
