bf1665be2f16d3a79f6ccae27a828294b71a1796
seancheng33 <seancheng33@gmail.com>
2019-04-04 15:34:34 +0800

    修改存活脚本的说明

--- python/host_alive.py
+++ python/host_alive.py
@@ -3,7 +3,7 @@
 @Author       : sean cheng
 @Email        : aya234@163.com
 @CreateTime   : 2019/4/4
-@Program      : 收集某一网段的IP段下的存活主机。
+@Program      : 收集某一网段的IP段下的存活主机。在linux系统下测试成功。可以正常使用。
 '''
 import os
 import sys
@@ -18,7 +18,7 @@ print(time.ctime())
 
 for host in range(1,255):
 	ip = '192.168.1.'+str(host)
-	pingaling = subprocess.Popen(['ping','-q','-c 2','r',ip],shell=False,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+	pingaling = subprocess.Popen(['ping','-q','-c 2','-r',ip],shell=False,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
 	print("Testing",ip,)
 
 	while 1:
